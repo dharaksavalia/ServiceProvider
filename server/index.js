2817b4d4e2d8882130be0f6b52454e9a141dfb72
Dharak Savalia <dharak.savalia@gmail.com>
2018-07-27 20:06:11 -0400

    passport google auth nodeman

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express=require('express');
+const passport=require('passport');
+const GoogleStrategy=require('passport-google-oauth20').Strategy;
 const app=express();
+const keys=require('./config/keys');
 
+passport.use(new GoogleStrategy({
+    clientID:keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+},(accessTocken,refreshToken,profile,done)=>{
+    console.log('accessToken',accessTocken);
+    console.log('refresh Token',refreshToken);
+    console.log('profile:',profile)
+}));
 
-app.get('/',(req,res)=>{
-    res.send({hi:'buddy'});
-});
+app.get('/auth/google',passport.authenticate('google',{
+    scope:['profile','email']
+}));
+
+app.get('/auth/google/callback',passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
