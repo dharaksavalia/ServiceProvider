b902c8e8fbec109c2c5fa290c756bfab7515a539
Dharak Savalia <dharak.savalia@gmail.com>
2018-07-28 15:49:22 -0400

    cookie mongoose finding user iwth id

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,14 +1,28 @@
 const passport=require('passport');
 const GoogleStrategy=require('passport-google-oauth20').Strategy;
+const mongoose =require('mongoose');
+const User = mongoose.model('users');
 const keys=require('../config/keys');
-
+passport.serializeUser((user,done)=>{
+    done(null,user.id);
+})
+passport.deserializeUser((id,done)=>{
+    User.findById(id).then(id).then(user=>{
+        done(null,user);
+    });
+});
 passport.use(new GoogleStrategy({
     clientID:keys.googleClientID,
     clientSecret:keys.googleClientSecret,
     callbackURL:'/auth/google/callback'
 },(accessTocken,refreshToken,profile,done)=>{
-    console.log('accessToken',accessTocken);
-    console.log('refresh Token',refreshToken);
-    console.log('profile:',profile)
+    User.findOne({googleId:profile.id}).then((existingUser)=>{
+        if(existingUser){
+            done(null,existingUser);
+        }else{
+            new User({googleId:profile.id}).save().then(user =>done(null,user));
+        }
+    })
+    
 }));
 
